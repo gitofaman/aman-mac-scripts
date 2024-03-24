bf78814aa730de8a132604f22e4da07f131732fb
gitofaman <amanofficial4252@gmail.com>
2024-03-24 19:48:58 +0530

    commit first

---
+++ src/Clients/Polyframe/rykers/script.js
@@ -0,0 +1,14 @@
+// $(document).ready(function(){
+//     const getTop = el => el.offsetTop + (el.offsetParent && getTop(el.offsetParent))
+//     const scrollToElementTop = (el, spaceFromTop) => {
+//         var scrollTo = getTop(el) - spaceFromTop
+//         window.scrollTo({
+//             top: scrollTo,
+//             behavior: 'smooth',
+//             })   
+//     }
+    
+//     $('.button.is-fixed-btn').on('click', function(){
+//         scrollToElementTop(document.querySelector('.w-nav'), 0)
+//     })
+// })
